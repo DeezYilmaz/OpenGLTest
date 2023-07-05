f03694df47019ff8861407d5555782cf2fffcc66
DeezYilmaz <98189271+DeezYilmaz@users.noreply.github.com>
2023-07-05 16:25:17 +0300

    Vertex buffer added

--- Main.cpp
+++ Main.cpp
@@ -43,6 +43,20 @@ int main()
     glfwSetMouseButtonCallback(window, mouse_button_callback);
 
 
+    float vertices[] = {
+        0.5f,0.0f,0.0f,
+       -0.5f,0.f,0.6f,
+        0.f,0.9f,-0.2f,
+    };
+
+    unsigned int VBO;
+    glGenBuffers(1,&VBO);
+    glBindBuffer(GL_ARRAY_BUFFER,VBO);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
+
+
+    //----------------------------
+
     while (!glfwWindowShouldClose(window)) {
 
         processInput(window);

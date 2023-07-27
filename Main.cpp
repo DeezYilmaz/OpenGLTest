64026093f7f778de9f5b631f82dc1da9b2f90a20
DeezYilmaz <98189271+DeezYilmaz@users.noreply.github.com>
2023-07-27 11:27:43 +0300

    Camera Fix

--- Main.cpp
+++ Main.cpp
@@ -6,10 +6,11 @@
 #include "Shader_s.h"
 #include <stb_image.h>
 
-#include <math.h>
+
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include "camera.h"
 #include "Cube.h"
 using namespace std;
 
@@ -28,8 +29,11 @@ int transfromUni;
 int MousePosUni;
 int textureUni;
 
-glm::mat4 viewMatrix;
-glm::mat4 projectionMatrix;
+Camera camera(glm::vec3(0.f,0.f,1.f));
+
+glm::mat4 view;
+glm::mat4 projection;
+
 int main()
 {
 	glfwInit();
@@ -59,6 +63,8 @@ int main()
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	glfwSetCursorPosCallback(window, cursor_position_callback);
 	
+	if (glfwRawMouseMotionSupported())
+		glfwSetInputMode(window, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
 
 
 
@@ -114,12 +120,6 @@ int main()
 	glEnable(GL_DEPTH_TEST);
 
 
-	glm::mat4 transformMatrix(1.0f);
-	viewMatrix=glm::mat4(1.0f);
-	viewMatrix = glm::lookAt(glm::vec3(1.0f, 1.0f, 5.0f), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
-	projectionMatrix= glm::perspective(80.0f, 800.0f / 600.0f, 0.1f, 100.0f);
-	glUniformMatrix4fv(projectUni, 1, GL_FALSE, glm::value_ptr(projectionMatrix * viewMatrix));
-
 
 	std::vector<unique_ptr<Drawable>> cubes;
 
@@ -143,6 +143,12 @@ int main()
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
 		processInput(window);
+
+		projection = glm::perspective(glm::radians(camera.Zoom), (float)WINDOW_WIDTH / (float) WINDOW_HEIGHT, 0.1f, 100.0f);
+		view = camera.GetViewMatrix();
+
+		glUniformMatrix4fv(projectUni, 1, GL_FALSE, glm::value_ptr(projection* view));
+
 		for (size_t i = 0; i < cubes.size(); i++)
 		{
 			cubes[i]->draw();
@@ -165,50 +171,80 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 }
 
 
-glm::vec3 rotationMatrix(0.f);
+
 float speed = 0.5f;
+
+
+float lastX = WINDOW_WIDTH / 2.0f;
+float lastY = WINDOW_HEIGHT / 2.0f;
+bool firstMouse = true;
+bool mouseActive = false;
+
 void processInput(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
 
-	
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, 0.1);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, 0.1);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, 0.1);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, 0.1);
+	if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS)
+		camera.ProcessKeyboard(DOWN, .1);
+	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+		camera.ProcessKeyboard(UP, .1);
 
 
-	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
-		viewMatrix = glm::translate(viewMatrix, glm::vec3( 0.f, 0.f, speed));
-		glUniformMatrix4fv(projectUni, 1, GL_FALSE, glm::value_ptr(projectionMatrix * viewMatrix));
+
+
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+
+		mouseActive = true;
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 	}
-	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
-		viewMatrix = glm::translate(viewMatrix, glm::vec3(-speed, 0.f, 0.f));
-		glUniformMatrix4fv(projectUni, 1, GL_FALSE, glm::value_ptr(projectionMatrix * viewMatrix));
+	else {
 
-	}
-	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
-		viewMatrix = glm::translate(viewMatrix, glm::vec3(0.f, 0.f, -speed));
-		glUniformMatrix4fv(projectUni, 1, GL_FALSE, glm::value_ptr(projectionMatrix * viewMatrix));
-
-	}
-	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
-		viewMatrix = glm::translate(viewMatrix, glm::vec3(speed, 0.f, 0.f));
-		glUniformMatrix4fv(projectUni, 1, GL_FALSE, glm::value_ptr(projectionMatrix * viewMatrix));
-
-	}
-	if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS) {
-		viewMatrix = glm::translate(viewMatrix, glm::vec3(0.f, -speed, 0.f ));
-		glUniformMatrix4fv(projectUni, 1, GL_FALSE, glm::value_ptr(projectionMatrix * viewMatrix));
-
-	}
-	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
-		viewMatrix = glm::translate(viewMatrix, glm::vec3(0.f, speed, 0.f));
-		glUniformMatrix4fv(projectUni, 1, GL_FALSE, glm::value_ptr(projectionMatrix * viewMatrix));
-
+		mouseActive = false;
+		firstMouse = true;
+		double xpos = (WINDOW_WIDTH / 2.0);
+		double ypos = (WINDOW_HEIGHT / 2.0);
+		glfwGetCursorPos(window, &xpos, &ypos);
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
 	}
 
+	if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS) {
+	}
+	if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS) {
+	}
 }
 
-void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
+
+
+
+void cursor_position_callback(GLFWwindow* window, double xposIn, double yposIn)
 {
-	MouseX = xpos / WINDOW_WIDTH;
-	MouseY = ypos / WINDOW_HEIGHT;
+
+	float xpos = static_cast<float>(xposIn);
+	float ypos = static_cast<float>(yposIn);
+
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	if(mouseActive)
+		camera.ProcessMouseMovement(xoffset, yoffset);
+		
 }
